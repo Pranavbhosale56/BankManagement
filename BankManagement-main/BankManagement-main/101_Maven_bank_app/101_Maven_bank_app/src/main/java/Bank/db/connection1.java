b14d13ee307e9f4741b1b0284093492dcdb642db
Pranav Bhosale <97728130+Pranavbhosale56@users.noreply.github.com>
2024-11-07 10:29:24 +0530

    Update connection1.java

--- BankManagement-main/BankManagement-main/101_Maven_bank_app/101_Maven_bank_app/src/main/java/Bank/db/connection1.java
+++ BankManagement-main/BankManagement-main/101_Maven_bank_app/101_Maven_bank_app/src/main/java/Bank/db/connection1.java
@@ -6,30 +6,6 @@ import java.sql.SQLException;
 import org.postgresql.ds.PGSimpleDataSource;
 
 
-/*
-public class connection1 {
-    private static final String url = "jdbc:postgresql://moneymint-4095.7s5.aws-ap-south-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full";
-    private static final String user = "dhirajdurande13";
-    private static final String password = "OSrL58LgPLCLcJN81XcIrw";
-
-    public static Connection getConnection() {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUser(user);
-        dataSource.setPassword(password);
-
-        try {
-            Connection conn = dataSource.getConnection();
-            System.out.println("Connection established successfully!");
-            return conn;
-        } catch (SQLException e) {
-            // Handle any potential SQLException
-            e.printStackTrace();
-            return null;
-        }
-    }
-}
-*/
 
 
 
@@ -68,39 +44,3 @@ public class connection1 {
 
 
 
-
-
-
-/*
-
-
-//Postagre SQL Connection 
-public class connection1 {
-  private static String driver = "org.postgresql.Driver";
-  private static String url = "jdbc:postgresql://localhost:5432/your_database_name";
-  private static String userName = "postgres";
-  private static String password = "DreamBig@18";
-
-  static {
-      try {
-          Class.forName(driver);
-      } catch (ClassNotFoundException e) {
-          e.printStackTrace();
-          throw new IllegalStateException("Failed to load JDBC driver.");
-      }
-  }
-
-  public static Connection getConnection() {
-      Connection conn = null;
-      try {
-          conn = DriverManager.getConnection(url, userName, password);
-          System.out.println("Connected to PostgreSQL database.");
-      } catch (SQLException e) {
-          e.printStackTrace();
-          throw new IllegalStateException("Failed to connect to the database.");
-      }
-      return conn;
-  }
-}
-
-*/
